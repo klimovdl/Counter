71deacd88e0ef981ed955b16eb22d6b7a3193a00
Danil K <klimov.danil@gmail.com>
2025-05-01 17:44:40 +0500

    Выпонена практическая задача

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var chetLabel: UILabel!
+    
+    @IBOutlet weak var activButtom: UIButton!
+    
+    private var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        counter = 0
+        updateLabel()
     }
 
-
+    @IBAction func ActivButtom(_ sender: UIButton) {
+        counter += 1
+        updateLabel()
+    }
+    private func updateLabel() {
+        chetLabel.text = "\(counter)"
+    }
+    
 }
 
